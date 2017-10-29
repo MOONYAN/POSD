61f6011ea19e896aa1c4f4ac49123e9f438bddcc
Boruto <t106598010@ntut.org.tw>
2017-10-29 18:11:24 +0800

    add test cases of Leaf

--- PrologInterpreter/Leaf.cpp
+++ PrologInterpreter/Leaf.cpp
@@ -1,12 +1,20 @@
 #include "Leaf.h"
 
 
-
-Leaf::Leaf()
+Leaf::Leaf(string tokenType, string tokenValue) :_tokenType(tokenType), _tokenValue(tokenValue)
 {
 }
 
-
 Leaf::~Leaf()
 {
+}
+
+string Leaf::getTokenType()
+{
+	return _tokenType;
+}
+
+string Leaf::getTokenValue()
+{
+	return _tokenValue;
 }
--- PrologInterpreter/Leaf.h
+++ PrologInterpreter/Leaf.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 class Leaf
 {
+private:
+	string _tokenType;
+	string _tokenValue;
 public:
-	Leaf();
+	Leaf(string tokenType, string tokenValue);
 	~Leaf();
+	string getTokenType();
+	string getTokenValue();
 };
 
