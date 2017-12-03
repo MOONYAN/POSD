3b664c979a2712774c579f5c6ab91c2639a584cd
Boruto <t106598010@ntut.org.tw>
2017-12-03 16:55:19 +0800

    add uts

--- PrologInterpreter/Source.cpp
+++ PrologInterpreter/Source.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
-//#include "global.h"
-//#include "utTerm.h"
-//#include "utStruct.h"
-//#include "utVariable.h"
-//#include "utList.h"
-//#include "utScanner.h"
-//#include "utLeaf.h"
-//#include "utParser.h"
-//#include "utBuilder.h"
+#include "global.h"
+#include "utTerm.h"
+#include "utStruct.h"
+#include "utVariable.h"
+#include "utList.h"
+#include "utScanner.h"
+#include "utLeaf.h"
+#include "utParser.h"
+#include "utBuilder.h"
 #include "utIterator.h"
 
 int main(int argc, char** argv)
