8174d8ecffa73a3847a77607fa741f10914b02ab
Boruto <t106598010@ntut.org.tw>
2017-12-03 14:57:54 +0800

    add class NullIterator

---
+++ PrologInterpreter/Iterator.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Term.h"
+
+template<class T>
+class Iterator
+{
+public:
+	virtual void first() = 0;
+	virtual void next() = 0;
+	virtual T currentItem() const = 0;
+	virtual bool isDone() const = 0;
+};
