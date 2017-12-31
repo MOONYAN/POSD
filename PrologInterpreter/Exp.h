593666b7d23e3350bc4b81edee69bb5242e80984
Boruto <t106598010@ntut.org.tw>
2017-12-31 13:45:38 +0800

    add Exp class

---
+++ PrologInterpreter/Exp.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Term.h"
+
+class Exp {
+public:
+	virtual bool evaluate() = 0;
+};
+
+class MatchExp : public Exp
+{
+public:
+	MatchExp(Term* left, Term* right) : _left(left), _right(right) {}
+
+	bool evaluate() override
+	{
+		return _left->match(*_right);
+	}
+
+private:
+	Term* _left;
+	Term* _right;
+};
+
+class ConjExp : public Exp
+{
+public:
+	ConjExp(Exp *left, Exp *right) : _left(left), _right(right) {}
+
+	bool evaluate() override
+	{
+		return (_left->evaluate() && _right->evaluate());
+	}
+
+private:
+	Exp * _left;
+	Exp * _right;
+};
+
+class DisjExp : public Exp
+{
+public:
+	DisjExp(Exp *left, Exp *right) : _left(left), _right(right) {}
+
+	bool evaluate() override
+	{
+		return (_left->evaluate() || _right->evaluate());
+	}
+
+private:
+	Exp * _left;
+	Exp * _right;
+};
